3235a6ca094df8f24eb5fd575281485b0d08ed80
hgcloud9 <howard.gilbrt@gmail.com>
2016-09-22 12:17:27 +0100

    comment email

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,22 @@
 class CommentsController < ApplicationController
+
+def comment_response(topic)
+       @topic=topic
+       @originator= @topic[:name]
+       @proposal=@topic[:title]
+       CommentMailer.new_comment_response(@topic,@originator).deliver_now
+  end
+
   def create
     @topic = Topic.find(params[:topic_id])
     @comment = @topic.comments.create(comment_params)
     redirect_to topic_path(@topic)
-#    redirect_to root_url
+    @originator= @topic[:name]
+    if @comment.save
+       comment_response(@topic)
+       flash[:info] = "New comment alert email sent."
+       redirect_to topic_path(@topic)
+    end
   end
 
   def destroy
